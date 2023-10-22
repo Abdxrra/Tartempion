584d96ae1ff9f0b0d9ef7b87617222f0023eeddb
Kenda04 <64617456+Kenda04@users.noreply.github.com>
2023-10-22 14:18:06 -0400

    Code formatté, Constante TEMPS_EPREUVE et méthode nouvelle_partie()

--- 5GP-Travail_pratique_1/monsieur_tartempion.py
+++ 5GP-Travail_pratique_1/monsieur_tartempion.py
@@ -30,13 +30,14 @@ import PySimpleGUI as gui
 from images import *
 from indicateurs import Indicateur
 
-
 NOM_FICHIER_SON_VICTOIRE = '522243__dzedenz__result-10.wav'
 NOM_FICHIER_SON_ERREUR = '409282__wertstahl__syserr1v1-in_thy_face_short.wav'
 NOM_FICHIER_SON_FIN_PARTIE = '173859__jivatma07__j1game_over_mono.wav'
 NOM_FICHIER_MUSIQUE_QUESTIONS = '550764__erokia__msfxp9-187_5-synth-loop-bpm-100.wav'
 
 NB_QUESTIONS = 21
+
+TEMPS_EPREUVE = 60
 
 TITRE = 'TITRE'
 TEMPS = 'TEMPS'
@@ -55,37 +56,43 @@ police_question = (gui.DEFAULT_FONT, 30, 'normal')
 police_reponses = (gui.DEFAULT_FONT, 20, 'normal')
 police_ou = (gui.DEFAULT_FONT, 20, 'italic')
 
-def splasher_equipe(temps_ms: int) -> None:
 
-    gui.Window('Monsieur Tartempion', [[gui.Image(data=equipe_base64())]], # transparent_color=gui.theme_background_color(),
+def splasher_equipe(temps_ms: int) -> None:
+    gui.Window('Monsieur Tartempion', [[gui.Image(data=equipe_base64())]],
+               # transparent_color=gui.theme_background_color(),
                no_titlebar=True, keep_on_top=True).read(timeout=temps_ms, close=True)
 
-def splacher_titre(delai: int, pardessus: bool) -> None:
 
-    gui.Window('Monsieur Tartempion', [[gui.Image(data=titre_base64())]], no_titlebar=True, keep_on_top=pardessus).read(timeout=delai, close=True)
+def splacher_titre(delai: int, pardessus: bool) -> None:
+    gui.Window('Monsieur Tartempion', [[gui.Image(data=titre_base64())]], no_titlebar=True, keep_on_top=pardessus).read(
+        timeout=delai, close=True)
 
 
 def afficher_jeu() -> gui.Window:
-
     title = [gui.Text('Monsieur Tartempion', key=TITRE, font=police_titre)]
 
-    temps = [[gui.Text('Temps restant', font=police_etiquettes, size=70, justification='center')], [gui.Text(str(60), key=TEMPS, font=police_temps)]]
+    temps = [[gui.Text('Temps restant', font=police_etiquettes, size=70, justification='center')],
+             [gui.Text(str(TEMPS_EPREUVE), key=TEMPS, font=police_temps)]]
 
-    boutons_reponse = [gui.Column([[gui.Button(key=BOUTON_GAUCHE, font=police_reponses, button_color=('white', gui.theme_background_color()),
-                   border_width=0, disabled=True, visible=True),
-        gui.Text(' ou ', key=OU, font=police_ou, text_color=gui.theme_background_color()),
-        gui.Button(key=BOUTON_DROIT, font=police_reponses, button_color=('white', gui.theme_background_color()),
-                   border_width=0, disabled=True, visible=True)]], element_justification='center')]
+    boutons_reponse = [gui.Column(
+        [[gui.Button(key=BOUTON_GAUCHE, font=police_reponses, button_color=('white', gui.theme_background_color()),
+                     border_width=0, disabled=True, visible=True),
+          gui.Text(' ou ', key=OU, font=police_ou, text_color=gui.theme_background_color()),
+          gui.Button(key=BOUTON_DROIT, font=police_reponses, button_color=('white', gui.theme_background_color()),
+                     border_width=0, disabled=True, visible=True)]], element_justification='center')]
 
     question = [gui.Text(' ', key=QUESTION, font=police_question)]
 
-    action = [gui.Button(image_data=bouton_jouer_base64(), key=BOUTON_ACTION, border_width=0, button_color=(gui.theme_background_color(), gui.theme_background_color()), pad=(0, 10)),
+    action = [gui.Button(image_data=bouton_jouer_base64(), key=BOUTON_ACTION, border_width=0,
+                         button_color=(gui.theme_background_color(), gui.theme_background_color()), pad=(0, 10)),
               gui.Image(data=bouton_inactif_base64(), key=IMAGE_BOUTON_INACTIF, visible=False, pad=(0, 10))]
 
-    indicateurs = [*[gui.Image(data=indicateur_vide_base64(), key=f'{INDICATEUR}-{i}', pad=(4, 10)) for i in range(NB_QUESTIONS)]]
+    indicateurs = [
+        *[gui.Image(data=indicateur_vide_base64(), key=f'{INDICATEUR}-{i}', pad=(4, 10)) for i in range(NB_QUESTIONS)]]
 
-    fenetre = gui.Window('Monsieur Tartempion', [temps, boutons_reponse, question, action, indicateurs], keep_on_top=True, element_padding=(0, 0),
-                        element_justification='center', resizable=False, finalize=True)
+    fenetre = gui.Window('Monsieur Tartempion', [temps, boutons_reponse, question, action, indicateurs],
+                         keep_on_top=True, element_padding=(0, 0),
+                         element_justification='center', resizable=False, finalize=True)
 
     return fenetre
 
@@ -98,7 +105,6 @@ def effacer_question_affichee(fenetre: gui.Window) -> None:
 
 
 def charger_questions(fichier_db: str) -> list:
-
     connexion = squirrel.connect(fichier_db)
 
     with connexion:
@@ -108,7 +114,6 @@ def charger_questions(fichier_db: str) -> list:
 
 
 def choisir_questions(banque: list, nombre: int) -> list:
-
     return [[question, Indicateur.VIDE] for question in random.choices(banque, k=nombre)]
 
 
@@ -117,14 +122,15 @@ def melanger_reponses(reponses: tuple) -> tuple:
 
 
 def splasher_echec(temps_ms: int) -> None:
-
-    gui.Window('Monsieur Tartempion', [[gui.Image(data=echec_base64())]], transparent_color=gui.theme_background_color(),
+    gui.Window('Monsieur Tartempion', [[gui.Image(data=echec_base64())]],
+               transparent_color=gui.theme_background_color(),
                no_titlebar=True, keep_on_top=True).read(timeout=temps_ms, close=True)
 
-def splasher_succes() -> None:
 
+def splasher_succes() -> None:
     gui.Window('Monsieur Tartempion', [[gui.Image(data=succes_base64())]], transparent_color="maroon2",
                no_titlebar=True, keep_on_top=True).read(timeout=3000, close=True)
+
 
 def afficher(fenetre: gui.Window, question: tuple) -> None:
     fenetre[QUESTION].update(question[0])
@@ -132,6 +138,7 @@ def afficher(fenetre: gui.Window, question: tuple) -> None:
     fenetre[BOUTON_GAUCHE].update(reponses[0], disabled=False, visible=True)
     fenetre[OU].update(text_color='white')
     fenetre[BOUTON_DROIT].update(reponses[1], disabled=False, visible=True)
+
 
 def effacer_question(fenetre: gui.Window) -> None:
     fenetre[QUESTION].update('')
@@ -157,9 +164,18 @@ def programme_principal() -> None:
     questions = choisir_questions(toutes_les_questions, NB_QUESTIONS)
 
     fenetre = afficher_jeu()
-    temps_restant = 60
+    temps_restant = TEMPS_EPREUVE
     prochaine_question = 0
     decompte_actif = False
+
+    def nouvelle_partie() -> None:
+        fenetre[BOUTON_ACTION].update(disabled=False, visible=True)
+        fenetre[IMAGE_BOUTON_INACTIF].update(visible=False)
+        temps_restant = TEMPS_EPREUVE
+        fenetre[TEMPS].update(str(temps_restant))
+        fenetre.un_hide()
+        questions = choisir_questions(toutes_les_questions, NB_QUESTIONS)
+        prochaine_question = 0
 
     quitter = False
     while not quitter:
@@ -179,14 +195,7 @@ def programme_principal() -> None:
                     # son_fin_partie.play()
                     # musique_questions_controles.stop()
                     splasher_echec(3000)
-
-                    fenetre[BOUTON_ACTION].update(disabled=False, visible=True)
-                    fenetre[IMAGE_BOUTON_INACTIF].update(visible=False)
-                    temps_restant = 60
-                    fenetre[TEMPS].update(str(temps_restant))
-                    fenetre.un_hide()
-                    questions = choisir_questions(toutes_les_questions, NB_QUESTIONS)
-                    prochaine_question = 0
+                    nouvelle_partie()
                     continue
 
         if event == BOUTON_ACTION:
@@ -198,7 +207,7 @@ def programme_principal() -> None:
             # musique_questions_controles = musique_questions.play()
         elif event == BOUTON_GAUCHE or event == BOUTON_DROIT:
             if (event == BOUTON_GAUCHE and fenetre[BOUTON_GAUCHE].get_text() != questions[prochaine_question][0][1]) or \
-               (event == BOUTON_DROIT and fenetre[BOUTON_DROIT].get_text() != questions[prochaine_question][0][1]):
+                    (event == BOUTON_DROIT and fenetre[BOUTON_DROIT].get_text() != questions[prochaine_question][0][1]):
                 # le joueur a choisi la mauvaise réponse
                 fenetre[f'{INDICATEUR}-{prochaine_question}'].update(data=indicateur_vert_base64())
                 questions[prochaine_question][1] = Indicateur.VERT
@@ -215,13 +224,7 @@ def programme_principal() -> None:
                     # musique_questions_controles.stop()
                     # son_victoire.play()
                     splasher_succes()
-                    fenetre[BOUTON_ACTION].update(disabled=False, visible=True)
-                    fenetre[IMAGE_BOUTON_INACTIF].update(visible=False)
-                    temps_restant = TEMPS_EPREUVE
-                    fenetre[TEMPS].update(str(temps_restant))
-                    fenetre.un_hide()
-                    questions = choisir_questions(toutes_les_questions, NB_QUESTIONS)
-                    prochaine_question = 0
+                    nouvelle_partie()
                     continue
             else:
                 # le joueur a choisi la bonne réponse
@@ -243,5 +246,6 @@ def programme_principal() -> None:
 
     fenetre.close()
     del fenetre
+
 
 programme_principal()
