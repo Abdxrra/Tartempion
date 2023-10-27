ceaa8ff889a128b730295c18ce55d3fbe4ce0d42
Abdxrra <78387960+Abdxrra@users.noreply.github.com>
2023-10-26 22:24:59 -0400

    ajout tests unitaires
    test melanger reponses
    test choisir nombre questions
    test verifier indicateur vide

--- 5GP-Travail_pratique_1/test_tartempion.py
+++ 5GP-Travail_pratique_1/test_tartempion.py
@@ -2,10 +2,15 @@ import unittest
 import PySimpleGUI as gui
 
 from indicateurs import Indicateur
-from monsieur_tartempion import fenetre_de_jeu, charger_questions, obtenir_activite_decompte, fermer_programme
+from monsieur_tartempion import fenetre_de_jeu, charger_questions, melanger_reponses, fermer_programme, choisir_questions, obtenir_activite_decompte
 
 class TestTartempion(unittest.TestCase):
     
+    def setUp(self):
+        self.toutes_les_questions = charger_questions("questions.bd")
+        self.nombre_de_questions = 3
+        self.liste_questions = choisir_questions(self.toutes_les_questions, self.nombre_de_questions)
+
     def test_creation_fenetre_de_jeu(self):
         fenetre = fenetre_de_jeu()
         self.assertIsInstance(fenetre, gui.Window)
@@ -13,15 +18,24 @@ class TestTartempion(unittest.TestCase):
     def test_charger_questions_bd(self):
         questions = charger_questions("questions.bd")
         self.assertIsInstance(questions, list)
-        
-    def test_decompte_inactif(self):
-        decompte_actif = obtenir_activite_decompte()
-        self.assertFalse(decompte_actif)
+    
+    def test_melanger_reponses(self):
+        reponses = ('A', 'B')
+        shuffled = melanger_reponses(reponses)
+        self.assertIn(shuffled, [('A', 'B'), ('B', 'A')])
         
     def test_fermer_fenetre(self):
         fenetre = fenetre_de_jeu()
         fermer_programme(fenetre)
-        self.assertNotIsInstance(fenetre, gui.Window)
+        self.assertTrue(fenetre.is_closed)
+
+    def test_choisir_nombre_questions(self):
+        longueur_liste_question = len(self.liste_questions)
+        self.assertEqual(longueur_liste_question, self.nombre_de_questions)
+
+    def test_verifier_indicateur_vide(self):
+        for question, indicator in self.liste_questions:
+            self.assertEqual(indicator, Indicateur.VIDE)
 
 
 if __name__ == '__main__':
